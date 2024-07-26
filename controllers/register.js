dd5b91b8e4836da976d899fec571d49ffaed7756
unknown <Aorus0804@gmail.com>
2024-07-27 01:30:22 +0700

    commit5

--- controllers/register.js
+++ controllers/register.js
@@ -4,13 +4,34 @@ document.querySelector('#frmSignUp').onsubmit = async function (e) {
     e.preventDefault();
     let member = new User();
     let arrInput = document.querySelectorAll('#frmSignUp .private');
-    for(let input of arrInput){
-            member[input.id]=input.value;
+  
+    for (let input of arrInput) {
+        member[input.id] = input.value;
     }
+    
+    let genderInputs = document.querySelectorAll('input[name="flexRadioDefault"]');
+    for (let radio of genderInputs) {
+        if (radio.checked) {
+            member.gender = (radio.value === 'true');
+            break;
+        }
+    }
+
     console.log(member);
-    let respone = await axios({
-        url: 'https://shop.cyberlearn.vn/api/Users/signup',
-        method: 'POST',
-        data: member
-    });
+    
+    try {
+      
+        let response = await axios({
+            url: 'https://shop.cyberlearn.vn/api/Users/signup',
+            method: 'POST',
+            data: member
+        });
+
+       
+        console.log('Đăng ký thành công:', response.data.content);
+
+    } catch (error) {
+      
+        console.error('Đăng ký thất bại:', error.response ? error.response.data : error.message);
+    }
 }
